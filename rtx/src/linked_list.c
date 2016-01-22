8b6ea82901a5c6746fabb0e71c6deed549987561
Ted Ying <yingted@gmail.com>
2016-01-22 18:45:53 -0500

    Pass the linked list test

--- rtx/src/linked_list.c
+++ rtx/src/linked_list.c
@@ -1,33 +1,76 @@
 #include <assert.h>
 #include "linked_list.h"
 
+static void ll_init_impl(ll_header_t *header) {
+	if (header->used_front == -1 && header->used_back != -1) {
+		const int capacity = header->used_back;
+		assert(capacity >= 0);
+
+		header->used_front = header->used_back = -1;
+		for (int i = 0; i < capacity - 1; ++i) {
+			header->next[i] = i + 1;
+		}
+		if (capacity > 0) {
+			header->next[capacity - 1] = -1;
+			header->free_front = 0;
+		} else {
+			header->free_front = -1;
+		}
+	}
+}
+
 size_t ll_size_impl(const ll_header_t *header) {
 	size_t size = 0;
-	for (int i = header->head; i != -1; i = header->next[i]) {
+	for (int i = header->used_front; i != -1; i = header->next[i]) {
 		++size;
 	}
 	return size;
 }
 
-int ll_push_front_impl(ll_header_t *header, int capacity) {
-	assert(ll_size_impl(header) < capacity);
-	...
-	return header->head;
+static int ll_alloc(ll_header_t *header) {
+	ll_init_impl(header);
+	const int cur = header->free_front;
+	assert(cur != -1);
+	header->free_front = header->next[cur];
+	return cur;
 }
 
-int ll_push_back_impl(ll_header_t *header, int capacity) {
-	assert(ll_size_impl(header) < capacity);
-	...
-	return header->head;
+int ll_push_front_impl(ll_header_t *header) {
+	const int cur = ll_alloc(header);
+	header->next[cur] = header->used_front;
+	header->used_front = cur;
+	if (header->used_back == -1) {
+		header->used_back = cur;
+	}
+	return cur;
 }
 
-int ll_pop_front_impl(ll_header_t *header) {
-	const int prev_head = header->head;
-	...
-	return prev_head;
+int ll_push_back_impl(ll_header_t *header) {
+	const int cur = ll_alloc(header);
+	if (header->used_front == -1) {
+		header->used_front = cur;
+	} else {
+		header->next[header->used_back] = cur;
+	}
+	header->next[cur] = -1;
+	header->used_back = cur;
+	return cur;
 }
 
 int ll_front_impl(ll_header_t *header) {
-	assert(header->head != -1);
-	return header->head;
+	const int cur = header->used_front;
+	assert(cur != -1);
+	return cur;
+}
+
+int ll_pop_front_impl(ll_header_t *header) {
+	const int cur = ll_front_impl(header);
+	header->used_front = header->next[cur];
+	if (header->used_front == -1) {
+		header->used_back = -1;
+	}
+
+	header->next[cur] = header->free_front;
+	header->free_front = cur;
+	return cur;
 }
--- rtx/src/linked_list.h
+++ rtx/src/linked_list.h
@@ -3,30 +3,35 @@
 
 #include <stddef.h>
 
+/*
+ * The header of a linked list.
+ * Each item is either in the used or free list.
+ * If the used_front == -1 but used_back != -1, then capacity is used_back and the list is uninitialized.
+ * Otherwise, used_back == -1 exactly when used_front == -1.
+ */
 typedef struct ll_header {
-	int head;
+	int used_front, used_back, free_front;
 	int next[0];
 } ll_header_t;
 
+// As a special case, if .used_front is negative, then the list needs to be initialized.
 #define LL_DECLARE(list, type, capacity_) \
 	struct { \
 		ll_header_t header; \
 		int next[capacity_]; \
 		type values[capacity_]; \
-	} list = { .header = { .head = -1 }, .next = { -1 } }
+	} list = { .header = { .used_front = -1, .used_back = capacity_ } }
 
 #define LL_CAPACITY(list) (sizeof((list).next) / sizeof((list).next[0]))
 
 size_t ll_size_impl(const ll_header_t *header);
 #define LL_SIZE(list) ll_size_impl(&(list).header)
 
-int ll_push_front_impl(ll_header_t *header, int capacity);
-#define LL_PUSH_FRONT(list, value) \
-	((list).values[ll_push_front_impl(&(list).header, LL_CAPACITY((list)))] = (value))
+int ll_push_front_impl(ll_header_t *header);
+#define LL_PUSH_FRONT(list, value) ((list).values[ll_push_front_impl(&(list).header)] = (value))
 
-int ll_push_back_impl(ll_header_t *header, int capacity);
-#define LL_PUSH_BACK(list, value) \
-	((list).values[ll_push_back_impl(&(list).header, LL_CAPACITY((list)))] = (value))
+int ll_push_back_impl(ll_header_t *header);
+#define LL_PUSH_BACK(list, value) ((list).values[ll_push_back_impl(&(list).header)] = (value))
 
 int ll_pop_front_impl(ll_header_t *header);
 #define LL_POP_FRONT(list) ((list).values[ll_pop_front_impl(&(list).header)])
@@ -36,7 +41,7 @@ int ll_front_impl(ll_header_t *header);
 
 #define LL_FOREACH(x, list) \
 	for ( \
-		int x ## _index = (list).header.head; \
+		int x ## _index = (list).header.used_front; \
 		x ## _index != -1 && (x = (list).values[x ## _index], 1); \
 		x ## _index = (list).next[x ## _index] \
 	)
