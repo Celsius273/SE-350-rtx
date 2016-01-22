cea8108e0f5228c83ba8de1297cb692452478523
Ted Ying <yingted@gmail.com>
2016-01-22 09:48:17 -0500

    Add Auto_Test test processes

--- rtx/src/usr_proc.h
+++ rtx/src/usr_proc.h
@@ -1,6 +1,6 @@
 /**
  * @file:   usr_proc.h
- * @brief:  Two user processes header file
+ * @brief:  Six user processes header file
  * @author: Yiqing Huang
  * @date:   2014/01/17
  */
@@ -11,5 +11,9 @@
 void set_test_procs(void);
 void proc1(void);
 void proc2(void);
+void proc3(void);
+void proc4(void);
+void proc5(void);
+void proc6(void);
 
 #endif /* USR_PROC_H_ */
