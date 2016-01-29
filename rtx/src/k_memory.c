97c46a4a2278f56a77ad45db44610e8c3886cac2
Pushpak Kumar <p23kumar@uwaterloo.ca>
2016-01-28 20:18:34 -0500

    Implemented request_memory_block, release_memory_block, and mem_init. Build issues (if there are any) will be fixed within next fews commits.

--- rtx/src/k_memory.c
+++ rtx/src/k_memory.c
@@ -1,11 +1,12 @@
 /**
  * @file:   k_memory.c
  * @brief:  kernel memory managment routines
- * @author: Yiqing Huang
- * @date:   2014/01/17
+ * @author: Pushpak Kumar
+ * @date:   2016/01/23
  */
 
 #include "k_memory.h"
+ #include "k_process.h"
 
 #ifdef DEBUG_0
 #include "printf.h"
@@ -15,6 +16,12 @@
 U32 *gp_stack; /* The last allocated stack low address. 8 bytes aligned */
                /* The first stack starts at the RAM high address */
 	       /* stack grows down. Fully decremental stack */
+
+//ll_header_t g_heap; /*list of available memory blocks*/
+
+LL_DECLARE(g_heap, mem_t*, 30);
+U8 *gp_heap_begin_addr;
+U8 *gp_heap_end_addr;
 
 /**
  * @brief: Initialize RAM as follows:
@@ -35,9 +42,9 @@ U32 *gp_stack; /* The last allocated stack low address. 8 bytes aligned */
           |        PCB pointers       |
           |---------------------------|<--- gp_pcbs
           |        Padding            |
-          |---------------------------|  
+          |---------------------------|
           |Image$$RW_IRAM1$$ZI$$Limit |
-          |...........................|          
+          |...........................|
           |       RTX  Image          |
           |                           |
 0x10000000+---------------------------+ Low Address
@@ -48,32 +55,33 @@ void memory_init(void)
 {
 	U8 *p_end = (U8 *)&Image$$RW_IRAM1$$ZI$$Limit;
 	int i;
-  
+
 	/* 4 bytes padding */
 	p_end += 4;
 
 	/* allocate memory for pcb pointers   */
 	gp_pcbs = (PCB **)p_end;
 	p_end += NUM_TEST_PROCS * sizeof(PCB *);
-  
 	for ( i = 0; i < NUM_TEST_PROCS; i++ ) {
 		gp_pcbs[i] = (PCB *)p_end;
-		p_end += sizeof(PCB); 
+		p_end += sizeof(PCB);
 	}
-#ifdef DEBUG_0  
-	printf("gp_pcbs[0] = 0x%x \n", gp_pcbs[0]);
-	printf("gp_pcbs[1] = 0x%x \n", gp_pcbs[1]);
-#endif
-	
+
 	/* prepare for alloc_stack() to allocate memory for stacks */
-	
 	gp_stack = (U32 *)RAM_END_ADDR;
 	if ((U32)gp_stack & 0x04) { /* 8 bytes alignment */
-		--gp_stack; 
+		--gp_stack;
 	}
-  
-	/* allocate memory for heap, not implemented yet*/
-  
+
+	/* allocate memory for heap*/
+	gp_heap_begin_addr = p_end;
+
+  	for( i = 0; i < NUM_MEM_BLOCKS; i++) {
+	    LL_PUSH_FRONT(g_heap) = &blocks[i];
+	    p_end += MEM_BLOCK_SIZE;
+	}
+
+	gp_heap_end_addr = p_end;
 }
 
 /**
@@ -83,31 +91,63 @@ void memory_init(void)
  * POST:  gp_stack is updated.
  */
 
-U32 *alloc_stack(U32 size_b) 
+U32 *alloc_stack(U32 size_b)
 {
 	U32 *sp;
 	sp = gp_stack; /* gp_stack is always 8 bytes aligned */
-	
+
 	/* update gp_stack */
 	gp_stack = (U32 *)((U8 *)sp - size_b);
-	
+
 	/* 8 bytes alignement adjustment to exception stack frame */
 	if ((U32)gp_stack & 0x04) {
-		--gp_stack; 
+		--gp_stack;
 	}
 	return sp;
 }
 
-void *k_request_memory_block(void) {
-#ifdef DEBUG_0 
-	printf("k_request_memory_block: entering...\n");
-#endif /* ! DEBUG_0 */
-	return (void *) NULL;
+void *k_request_memory_block(void)
+{
+	U8 *p_mem_blk = NULL;
+
+	while(LL_SIZE(g_heap) == 0) {
+		k_enqueue_blocked_on_resource_process(gp_current_process);
+		k_release_processor();
+	}
+
+	p_mem_blk = (U8 *)LL_POP_FRONT(g_heap);
+	return (void *)p_mem_blk;
 }
 
 int k_release_memory_block(void *p_mem_blk) {
-#ifdef DEBUG_0 
-	printf("k_release_memory_block: releasing block @ 0x%x\n", p_mem_blk);
-#endif /* ! DEBUG_0 */
+	//if memory block pointer is not valid return error
+	if(k_release_memory_block_valid(p_mem_blk) == RTX_OK){
+		PCB *p_blocked_pcb = k_dequeue_blocked_on_resource_process();
+		if(p_blocked_pcb != NULL){
+			p_blocked_pcb->m_state = READY;
+			k_enqueue_ready_process(p_blocked_pcb);
+			return k_release_processor();
+		}
+	}
+	else{
+		return RTX_ERR;
+	}
 	return RTX_OK;
+}
+
+int k_release_memory_block_valid(void *p_mem_blk)
+{
+  mem_t *p_mem = NULL;
+  if(p_mem_blk == NULL){
+    return RTX_ERR;
+  }
+
+  if((U8* )p_mem < gp_heap_begin_addr || (U8 *)p_mem > gp_heap_end_addr) {
+    return RTX_ERR;
+  }
+  if(((U8* )p_node - gp_heap_begin_addr) % MEM_BLOCK_SIZE != 0){
+    return RTX_ERR;
+  }
+  LL_PUSH_BACK(g_heap, p_node)
+  return RTX_OK;
 }
