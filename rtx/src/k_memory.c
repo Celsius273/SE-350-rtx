ee5a01599306f9fced4f25833a62395ef36b27d4
Ted Ying <yingted@gmail.com>
2016-02-05 20:02:19 -0500

    Remove an assert

--- rtx/src/k_memory.c
+++ rtx/src/k_memory.c
@@ -118,7 +118,6 @@ void *k_request_memory_block(void)
         gp_current_process->m_state = BLOCKED_ON_RESOURCE;
 		k_release_processor();
         assert(gp_current_process->m_state == RDY);
-        assert(LL_SIZE(g_heap) != 0);
 	}
 
 	p_mem_blk = (U8 *)LL_POP_FRONT(g_heap);
