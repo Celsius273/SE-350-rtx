26022b124c3a2714bf26d6e28d41804bc6ef18a2
Ted Ying <yingted@gmail.com>
2016-03-05 14:09:46 -0500

    Add a bunch of processes

--- rtx/src/k_process.c
+++ rtx/src/k_process.c
@@ -61,9 +61,14 @@ static void infinite_loop(void)
 
 /* process initialization table */
 const static PROC_INIT g_proc_table[] = {
-	// m_pid      m_priority      m_stack_size  mpf_start_pc
-	{PID_NULL,    NULL_PRIO,      0x100,        &infinite_loop},
-	{PID_CLOCK,   HIGHEST,        0x100,        &proc_clock},
+	// m_pid           m_priority      m_stack_size  mpf_start_pc
+	{PID_NULL,         NULL_PRIO,      0x100,        &infinite_loop},
+	{PID_CLOCK,        HIGHEST,        0x100,        &proc_clock},
+	// TODO add these
+	{PID_KCD,          NULL_PRIO,      0x100,        &infinite_loop},
+	{PID_CRT,          NULL_PRIO,      0x100,        &infinite_loop},
+	{PID_TIMER_IPROC,  NULL_PRIO,      0x100,        &infinite_loop},
+	{PID_UART_IPROC,   NULL_PRIO,      0x100,        &infinite_loop},
 };
 extern PROC_INIT g_test_procs[NUM_TEST_PROCS];
 
