5ae573662b5f025e1b0af98b843e86fe5ba64c74
Ted Ying <yingted@gmail.com>
2016-03-05 14:49:50 -0500

    Use void* instead of int

--- rtx/src/disallow_k.h
+++ rtx/src/disallow_k.h
@@ -1,13 +1,13 @@
 #ifndef k_rtx_init
 
-#define k_delayed_send ((unsigned int)k_delayed_send)
-#define k_get_process_priority ((unsigned int)k_get_process_priority)
-#define k_receive_message ((unsigned int)k_receive_message)
-#define k_release_memory_block ((unsigned int)k_release_memory_block)
-#define k_release_processor ((unsigned int)k_release_processor)
-#define k_request_memory_block ((unsigned int)k_request_memory_block)
-#define k_rtx_init ((unsigned int)k_rtx_init)
-#define k_send_message ((unsigned int)k_send_message)
-#define k_set_process_priority ((unsigned int)k_set_process_priority)
+#define k_delayed_send ((void *)k_delayed_send)
+#define k_get_process_priority ((void *)k_get_process_priority)
+#define k_receive_message ((void *)k_receive_message)
+#define k_release_memory_block ((void *)k_release_memory_block)
+#define k_release_processor ((void *)k_release_processor)
+#define k_request_memory_block ((void *)k_request_memory_block)
+#define k_rtx_init ((void *)k_rtx_init)
+#define k_send_message ((void *)k_send_message)
+#define k_set_process_priority ((void *)k_set_process_priority)
 
 #endif
