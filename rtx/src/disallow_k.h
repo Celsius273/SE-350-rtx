495ec43ae2ee4319f3b18b31e5bb80c2ebfe5fa8
Ted Ying <yingted@gmail.com>
2016-03-05 14:48:46 -0500

    Use int instead of const char*

--- rtx/src/disallow_k.h
+++ rtx/src/disallow_k.h
@@ -1,13 +1,13 @@
 #ifndef k_rtx_init
 
-#define k_delayed_send "Can't call k_delayed_send"
-#define k_get_process_priority "Can't call k_get_process_priority"
-#define k_receive_message "Can't call k_receive_message"
-#define k_release_memory_block "Can't call k_release_memory_block"
-#define k_release_processor "Can't call k_release_processor"
-#define k_request_memory_block "Can't call k_request_memory_block"
-#define k_rtx_init "Can't call k_rtx_init"
-#define k_send_message "Can't call k_send_message"
-#define k_set_process_priority "Can't call k_set_process_priority"
+#define k_delayed_send ((unsigned int)k_delayed_send)
+#define k_get_process_priority ((unsigned int)k_get_process_priority)
+#define k_receive_message ((unsigned int)k_receive_message)
+#define k_release_memory_block ((unsigned int)k_release_memory_block)
+#define k_release_processor ((unsigned int)k_release_processor)
+#define k_request_memory_block ((unsigned int)k_request_memory_block)
+#define k_rtx_init ((unsigned int)k_rtx_init)
+#define k_send_message ((unsigned int)k_send_message)
+#define k_set_process_priority ((unsigned int)k_set_process_priority)
 
 #endif
